2131e802f0933e73cd39abedb02e153be005a855
GRGeoperi <ggonzalezr2100@alumno.ipn.mx>
2023-10-26 23:47:25 -0600

    Refactorizar binario de 4 bits a display

--- bin_siete_segmentos.vhdl
+++ bin_siete_segmentos.vhdl
@@ -6,56 +6,56 @@ use ieee.std_logic_1164.all;
 entity DATOS is port
 (
     -- 4 bits para la entrada
-    B: in std_logic_vector(3 downto 0);
+    BINARIO: in std_logic_vector(3 downto 0);
     -- 7 bits para la salida
-    F: out std_logic_vector(6 downto 0)
+    SEGMENTO: out std_logic_vector(6 downto 0)
 );
 end DATOS;
 
 architecture DISPLAY of DATOS is
 begin
-    -- Cuando el segmento 'a' se enciende
-    F(6) <= (not B(2) and not B(0)) or
-            (B(2) and B(1)) or
-            (not B(3) and B(1)) or
-            (not B(3) and B(2) and B(0)) or
-            (B(3) and not B(2) and not B(1)) or
-            (B(3) and not B(1) and not B(0));
-    -- Cuando el segmento 'b' se enciende
-    F(5) <= (not B(2) and not B(1)) or
-            (not B(3) and not B(2)) or
-            (not B(2) and not B(0)) or
-            (not B(3) and B(1) and B(0)) or
-            (B(3) and not B(1) and B(0)) or
-            (not B(3) and not B(1) and not B(0));
-    -- Cuando el segmento 'c' se enciende
-    F(4) <= (not B(3) and B(2)) or
-            (not B(1) and B(0)) or
-            (B(3) and not B(2)) or
-            (not B(3) and not B(1)) or
-            (not B(3) and B(0));
-    -- Cuando el segmento 'd' se enciende
-    F(3) <= (B(2) and not B(1) and B(0)) or
-            (not B(2) and not B(1) and not B(0)) or
-            (not B(3) and not B(2) and B(1)) or
-            (B(2) and B(1) and not B(0)) or
-            (not B(2) and B(1) and B(0)) or
-            (B(3) and B(2) and not B(1));
-    -- Cuando el segmento 'e' se enciende
-    F(2) <= (B(3) and B(2)) or
-            (B(1) and not B(0)) or
-            (not B(2) and not B(0)) or
-            (B(3) and B(1));
-    -- Cuando el segmento 'f' se enciende
-    F(1) <= (not B(1) and not B(0)) or
-            (B(3) and not B(2)) or
-            (B(3) and B(1)) or
-            (not B(3) and B(2) and not B(1)) or
-            (B(2) and B(1) and not B(0));
-    -- Cuando el segmento 'g' se enciende
-    F(0) <= (B(3) and not B(2)) or
-            (B(1) and not B(0)) or
-            (B(3) and B(0)) or
-            (not B(3) and B(2) and not B(1)) or
-            (not B(3) and not B(2) and B(1));
+    -- Cuando 'a' se enciende
+    SEGMENTO(6) <= (not BINARIO(2) and not BINARIO(0)) or
+            (BINARIO(2) and BINARIO(1)) or
+            (not BINARIO(3) and BINARIO(1)) or
+            (not BINARIO(3) and BINARIO(2) and BINARIO(0)) or
+            (BINARIO(3) and not BINARIO(2) and not BINARIO(1)) or
+            (BINARIO(3) and not BINARIO(1) and not BINARIO(0));
+    -- Cuando 'b' se enciende
+    SEGMENTO(5) <= (not BINARIO(2) and not BINARIO(1)) or
+            (not BINARIO(3) and not BINARIO(2)) or
+            (not BINARIO(2) and not BINARIO(0)) or
+            (not BINARIO(3) and BINARIO(1) and BINARIO(0)) or
+            (BINARIO(3) and not BINARIO(1) and BINARIO(0)) or
+            (not BINARIO(3) and not BINARIO(1) and not BINARIO(0));
+    -- Cuando 'c' se enciende
+    SEGMENTO(4) <= (not BINARIO(3) and BINARIO(2)) or
+            (not BINARIO(1) and BINARIO(0)) or
+            (BINARIO(3) and not BINARIO(2)) or
+            (not BINARIO(3) and not BINARIO(1)) or
+            (not BINARIO(3) and BINARIO(0));
+    -- Cuando 'd' se enciende
+    SEGMENTO(3) <= (BINARIO(2) and not BINARIO(1) and BINARIO(0)) or
+            (not BINARIO(2) and not BINARIO(1) and not BINARIO(0)) or
+            (not BINARIO(3) and not BINARIO(2) and BINARIO(1)) or
+            (BINARIO(2) and BINARIO(1) and not BINARIO(0)) or
+            (not BINARIO(2) and BINARIO(1) and BINARIO(0)) or
+            (BINARIO(3) and BINARIO(2) and not BINARIO(1));
+    -- Cuando 'e' se enciende
+    SEGMENTO(2) <= (BINARIO(3) and BINARIO(2)) or
+            (BINARIO(1) and not BINARIO(0)) or
+            (not BINARIO(2) and not BINARIO(0)) or
+            (BINARIO(3) and BINARIO(1));
+    -- Cuando 'f' se enciende
+    SEGMENTO(1) <= (not BINARIO(1) and not BINARIO(0)) or
+            (BINARIO(3) and not BINARIO(2)) or
+            (BINARIO(3) and BINARIO(1)) or
+            (not BINARIO(3) and BINARIO(2) and not BINARIO(1)) or
+            (BINARIO(2) and BINARIO(1) and not BINARIO(0));
+    -- Cuando 'g' se enciende
+    SEGMENTO(0) <= (BINARIO(3) and not BINARIO(2)) or
+            (BINARIO(1) and not BINARIO(0)) or
+            (BINARIO(3) and BINARIO(0)) or
+            (not BINARIO(3) and BINARIO(2) and not BINARIO(1)) or
+            (not BINARIO(3) and not BINARIO(2) and BINARIO(1));
 end DISPLAY;
